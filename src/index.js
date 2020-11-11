c1199dcfe0c39c9630d8c249e067767157e3b8b3
Matt George <mbgeorge48@gmail.com>
2020-11-11 16:06:53 +0000

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Weapons extends React.Component{
+  render() {
+    return (
+      <p>Weapons</p>
+    );
+  }
+}
+
+class Armor extends React.Component{
+  render() {
+    return (
+      <p>Armor</p>
+    );
+  }
+}
+
+class Accessories extends React.Component{
+  render() {
+    return (
+      <p>Accessories</p>
+    );
+  }
+}
+
+class Buffs extends React.Component{
+  render() {
+    return (
+      <p>Buffs</p>
+    );
+  }
+}
+
+class Mounts extends React.Component{
+  render() {
+    return (
+      <p>Mounts</p>
+    );
+  }
+}
+
+class Light extends React.Component{
+  render() {
+    return (
+      <p>Light</p>
+    );
+  }
+}
+
+class TerrariaClasses extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Weapons />
+          <Armor />
+          <Accessories />
+          <Buffs />
+          <Mounts />
+          <Light />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TerrariaClasses />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
