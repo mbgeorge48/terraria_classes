3762db9d5cddcc015676507c99e8807783c2f532
Matt George <mbgeorge48@gmail.com>
2020-11-14 01:56:04 +0000

    Added scss, class buttons, colours for the equipment

--- src/index.js
+++ src/index.js
@@ -3,41 +3,65 @@ import ReactDOM from "react-dom";
 import Slider from "@material-ui/core/Slider";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import "./index.scss";
 
 class Weapons extends Component {
   render() {
-    return <h4>Weapons</h4>;
+    return (
+      <div class="col-md weapons">
+        <h4>Weapons</h4>
+      </div>
+    );
   }
 }
 
 class Armor extends Component {
   render() {
-    return <h4>Armor</h4>;
+    return (
+      <div class="col-md armor">
+        <h4>Armor</h4>
+      </div>
+    );
   }
 }
 
 class Accessories extends Component {
   render() {
-    return <h4>Accessories</h4>;
+    return (
+      <div class="col-md accessories">
+        <h4>Accessories</h4>
+      </div>
+    );
   }
 }
 
 class Buffs extends Component {
   render() {
-    return <h4>Buffs</h4>;
+    return (
+      <div class="col-md buffs">
+        <h4>Buffs</h4>
+      </div>
+    );
   }
 }
 
 class Mounts extends Component {
   render() {
-    return <h4>Mounts</h4>;
+    return (
+      <div class="col-md mounts">
+        <h4>Mounts</h4>
+      </div>
+    );
   }
 }
 
 class Lights extends Component {
   render() {
-    return <h4>Lights</h4>;
+    return (
+      <div class="col-md lights">
+        <h4>Lights</h4>
+      </div>
+    );
   }
 }
 
@@ -86,16 +110,43 @@ class ProgressBar extends Component {
 
   render() {
     return (
-      <div>
+      <div class="top-buffer">
         <h4>Select what stage of the game you're on</h4>
-        <Slider
-          defaultValue={0}
-          valueLabelDisplay="auto"
-          step={1}
-          marks={this.marks}
-          min={0}
-          max={6}
-        />
+
+        <div class="row">
+          <Slider
+            defaultValue={0}
+            valueLabelDisplay="auto"
+            step={1}
+            marks={this.marks}
+            min={0}
+            max={6}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Category extends Component {
+  render() {
+    return (
+      <div class="top-buffer">
+        <h4>Select your current class</h4>
+        <div class="row ">
+          <div class="col-md">
+            <button class="btn btn-dark btn-lg btn-block">Melee</button>
+          </div>
+          <div class="col-md">
+            <button class="btn btn-dark btn-lg btn-block">Ranged</button>
+          </div>
+          <div class="col-md">
+            <button class="btn btn-dark btn-lg btn-block">Magic</button>
+          </div>
+          <div class="col-md">
+            <button class="btn btn-dark btn-lg btn-block">Summoner</button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -104,30 +155,18 @@ class ProgressBar extends Component {
 class Equipment extends Component {
   render() {
     return (
-      <div className="equipment">
-        <div class="row top-buffer">
-          <div class="col weapons">
-            <Weapons />
-          </div>
-          <div class="col armor">
-            <Armor />
-          </div>
+      <div className="equipment" class="container top-buffer">
+        <div class="row">
+          <Weapons />
+          <Armor />
         </div>
-        <div class="row top-buffer">
-          <div class="col accessories">
-            <Accessories />
-          </div>
-          <div class="col buffs">
-            <Buffs />
-          </div>
+        <div class="row">
+          <Accessories />
+          <Buffs />
         </div>
-        <div class="row top-buffer">
-          <div class="col mounts">
-            <Mounts />
-          </div>
-          <div class="col lights">
-            <Lights />
-          </div>
+        <div class="row">
+          <Mounts />
+          <Lights />
         </div>
       </div>
     );
@@ -143,11 +182,8 @@ class TerrariaClasses extends Component {
     return (
       <div class="container">
         <PageHeading />
-        <div class="row top-buffer">
-          <div class="col">
-            <ProgressBar />
-          </div>
-        </div>
+        <ProgressBar />
+        <Category />
         <div class="row top-buffer">
           <Equipment />
         </div>
