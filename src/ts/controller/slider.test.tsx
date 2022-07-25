0f71e347586ea5842d9acf8d94cbc77248326f88
Matt George <mbgeorge48@gmail.com>
2022-07-25 22:53:54 +0100

    Test the slider

---
+++ src/ts/controller/slider.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import Slider from "./slider";
+
+it("should render the game stage slider", () => {
+  const selectedRole='test'
+
+  render(
+    <Slider
+      min={0}
+      max={1}
+      labelText="Slider Label"
+      rangeColour={`accent-${selectedRole}`}
+      onGameStageChange={jest.fn()}
+  />
+  );
+
+  const slider = screen.getByRole("slider") 
+  expect(slider).not.toBeNull();
+  expect(screen.getByText("Slider Label")).not.toBeNull();
+  expect(slider.classList.contains("accent-test")).toBe(true);
+});
+
+it("should call the game stage change function when the slider is updated", () => {
+  const onGameStageChange = jest.fn();
+
+  render(
+    <Slider
+    min={0}
+    max={6}
+    labelText=""
+    rangeColour=""
+    onGameStageChange={onGameStageChange}
+  />
+  );
+
+  fireEvent.change(screen.getByRole("slider"), { target: { value: '2' } });
+  expect(onGameStageChange).toHaveBeenCalledTimes(1);
+});
