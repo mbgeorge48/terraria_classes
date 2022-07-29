51a1987cc0d0123def435d8d6bb6e6bece08ffbc
Matt George <mbgeorge48@gmail.com>
2022-07-29 20:23:05 +0100

    Add extra roles

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,21 +1,31 @@
 export type RoleChangeHandler = (role: Role) => void;
 export type GameStageChangeHandler = (index: number) => void;
 
-export type ItemCategory= "weapons"
-| "armor"
-| "accessories"
-| "buffs/potions"
-| "mounts"
-| "lights";
+export type ItemCategory =
+  | "weapons"
+  | "armor"
+  | "accessories"
+  | "buffs/potions"
+  | "mounts"
+  | "lights";
 
-export type Role= "melee"|"ranged"|"magic"|"summoner" | "mixed" | undefined
+export type Role =
+  | "melee"
+  | "ranged"
+  | "magic"
+  | "summoner"
+  | "mixed"
+  | "healer"
+  | "mixed"
+  | "tank"
+  | undefined;
 
-export type items = ReadonlyArray<item>
+export type items = ReadonlyArray<item>;
 export interface item {
-    name:string;
-    role:Role;
-    url:string;
-    imgPath:string;
-    category:ItemCategory;
-    gameStageAvailable:number;
+  name: string;
+  role: Role;
+  url: string;
+  imgPath: string;
+  category: ItemCategory;
+  gameStageAvailable: number;
 }
