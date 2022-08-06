0c6e2e200a3c552ba3a8c528e37842f1ba2d167a
Matt George <matt@dabapps.com>
2022-08-06 10:19:26 +0100

    Add constants for different css classes

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,9 +1,49 @@
-export const gameStages: {[key:number]:string} = {
-    0:"Pre-bosses",
-    1:"Pre-Hardmode",
-    2:"Pre-mech bosses",
-    3:"Pre-Plantera",
-    4:"Pre-Golem",
-    5:"Pre-Lunar Events",
-    6:"Endgame",
-}
+export const gameStages: { [key: number]: string } = {
+  0: "Pre-bosses",
+  1: "Pre-Hardmode",
+  2: "Pre-mech bosses",
+  3: "Pre-Plantera",
+  4: "Pre-Golem",
+  5: "Pre-Lunar Events",
+  6: "Endgame",
+};
+
+export const borderColour = {
+  melee: "border-melee",
+  ranged: "border-ranged",
+  magic: "border-magic",
+  summoner: "border-summoner",
+  mixed: "border-mixed",
+  tank: "border-tank",
+  healer: "border-healer",
+};
+
+export const bgColour = {
+  melee: "bg-melee",
+  ranged: "bg-ranged",
+  magic: "bg-magic",
+  summoner: "bg-summoner",
+  mixed: "bg-mixed",
+  tank: "bg-tank",
+  healer: "bg-healer",
+};
+
+export const accentColour = {
+  melee: "accent-melee",
+  ranged: "accent-ranged",
+  magic: "accent-magic",
+  summoner: "accent-summoner",
+  mixed: "accent-mixed",
+  tank: "accent-tank",
+  healer: "accent-healer",
+};
+
+export const decorationColour = {
+  melee: "decoration-melee",
+  ranged: "decoration-ranged",
+  magic: "decoration-magic",
+  summoner: "decoration-summoner",
+  mixed: "decoration-mixed",
+  tank: "decoration-tank",
+  healer: "decoration-healer",
+};
