0261aa55ff45bd969d2803e5ce698d62ff183278
Matt George <mbgeorge48@gmail.com>
2022-08-07 21:34:11 +0100

    Add in a credit button

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./ts/body";
 import "./css/index.css";
+import { CreditPopover } from "./ts/credit-popover";
 
-const backgrounds = [
-  "bg-1",
-  "bg-2",
-  "bg-3",
-  "bg-4",
-  "bg-5",
-  "bg-6",
-  "bg-7",
-  "bg-8",
-  "bg-9",
-];
+const backgrounds = ["bg-1", "bg-2", "bg-3", "bg-4", "bg-5", "bg-6"];
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -28,5 +19,8 @@ root.render(
     )}
   >
     <Body />
+    <div className="ml-6 mt-auto mb-4">
+      <CreditPopover />
+    </div>
   </div>
 );
---
+++ src/ts/credit-popover.tsx
@@ -0,0 +1,44 @@
+import { Popover } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
+
+import React from "react";
+
+export function CreditPopover() {
+  return (
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={`flex flex-row  border-2 border-gray-500 bg-white p-2 hover:border-yellow-400
+              ${open ? "rounded-b-xl" : "rounded-xl"}`}
+          >
+            <span>Credits</span>
+            <ChevronUpIcon
+              className={`w-5 h-5 self-center ${
+                open ? "rotate-180 transform" : undefined
+              }`}
+            />
+          </Popover.Button>
+          <Popover.Panel
+            className={`absolute z-10 -top-16 py-2 px-4 border-2 border-gray-400 bg-white flex flex-col
+            ${open ? "rounded-t-xl rounded-r-xl" : undefined}`}
+          >
+            <span>Designed and created by me, Matt G</span>
+            <div>
+              <span>Checkout the </span>
+              <a
+                href="https://github.com"
+                target="_blank"
+                rel="noreferrer"
+                className="underline decoration-4 decoration-yellow-400"
+              >
+                GitHub repo
+              </a>
+              <span> for more info</span>
+            </div>
+          </Popover.Panel>
+        </>
+      )}
+    </Popover>
+  );
+}
