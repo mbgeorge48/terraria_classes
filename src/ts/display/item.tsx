4a3adf24a7d3a893f5982e12effbe1d1f743bf21
Matt George <mbgeorge48@gmail.com>
2022-08-15 21:51:28 +0100

    Add padding to the item

--- src/ts/display/item.tsx
+++ src/ts/display/item.tsx
@@ -13,7 +13,7 @@ const Item: React.FC<Props> = ({ item, selectedRole }) => {
   return (
     <a
       className={classNames(
-        "flex flex-row space-x-4 w-fit place-items-center decoration-4 underline-offset-2 hover:underline",
+        "flex flex-row space-x-4 px-2 w-fit place-items-center decoration-4 underline-offset-2 hover:underline",
         selectedRole ? roleClasses[selectedRole].decoration : undefined
       )}
       href={item.url}
@@ -23,7 +23,7 @@ const Item: React.FC<Props> = ({ item, selectedRole }) => {
       <img
         src={item.imgPath}
         alt={item.name}
-        className="w-10 h-10 object-contain"
+        className="object-contain w-10 h-10"
       />
       <p className="text-lg">{item.name}</p>
     </a>
