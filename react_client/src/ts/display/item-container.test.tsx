102b4d779965538ed4543b1afb9bb39e003899e6
Matt George <mbgeorge48@gmail.com>
2023-02-12 22:51:48 +0000

    Add proxy and fix test imports

--- react_client/src/ts/display/item-container.test.tsx
+++ react_client/src/ts/display/item-container.test.tsx
@@ -1,29 +1,29 @@
 import { render, screen } from "@testing-library/react";
 
 import React from "react";
-import { fakeData } from "../../__tests__/utils.test";
+import { fakeData } from "../utils.test";
 
-import ItemContainer from "../item-container";
+import ItemContainer from "./item-container";
 
 it("should render nothing when there is no selected role", () => {
-  const { container } = render(
-    <ItemContainer selectedRole={undefined} itemCategory={"weapons"} />
-  );
+    const { container } = render(
+        <ItemContainer selectedRole={undefined} itemCategory={"weapons"} />
+    );
 
-  expect(container.childElementCount).toEqual(0);
+    expect(container.childElementCount).toEqual(0);
 });
 
 it("should render an item container when there is a selected role", () => {
-  const { container } = render(
-    <ItemContainer
-      selectedRole={"melee"}
-      itemCategory={"weapons"}
-      data={fakeData}
-    />
-  );
-  expect(container).not.toBeNull();
-  expect(screen.getByText("weapons")).not.toBeNull();
-  expect(screen.getByText(fakeData[0].name)).not.toBeNull();
-  expect(screen.getAllByRole("img")).toHaveLength(fakeData.length);
-  expect(screen.getAllByRole("link")).toHaveLength(fakeData.length);
+    const { container } = render(
+        <ItemContainer
+            selectedRole={"melee"}
+            itemCategory={"weapons"}
+            data={fakeData}
+        />
+    );
+    expect(container).not.toBeNull();
+    expect(screen.getByText("weapons")).not.toBeNull();
+    expect(screen.getByText(fakeData[0].name)).not.toBeNull();
+    expect(screen.getAllByRole("img")).toHaveLength(fakeData.length);
+    expect(screen.getAllByRole("link")).toHaveLength(fakeData.length);
 });
