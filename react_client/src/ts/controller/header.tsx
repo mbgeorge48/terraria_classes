d795f1c5d08eb61d157888732c4653e97518675c
Matt George <mbgeorge48@gmail.com>
2023-04-05 21:02:01 +0100

    Tweak styles

--- react_client/src/ts/controller/header.tsx
+++ react_client/src/ts/controller/header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Header: React.FC = () => (
-  <header className="capitalize p-4 lg:justify-start lg:space-x-0 font-terraria space-x-2 justify-center flex-row lg:flex-col flex text-3xl bg-white/90 lg:max-w-min font-bold m-2 rounded-xl">
-    <h1>terraria</h1>
-    <h1>classes</h1>
-    <h1>guide</h1>
-  </header>
+    <header className="flex flex-row justify-center p-4 m-2 space-x-2 text-3xl font-bold leading-none capitalize lg:justify-start lg:space-x-0 font-terraria lg:flex-col bg-white/90 lg:max-w-min rounded-xl">
+        <h1>terraria</h1>
+        <h1>classes</h1>
+        <h1>guide</h1>
+    </header>
 );
 
 export default Header;
--- react_client/src/ts/display/item-container.tsx
+++ react_client/src/ts/display/item-container.tsx
@@ -7,50 +7,54 @@ import { ItemCategory, items, Role } from "../types";
 import Item from "./item";
 
 interface Props {
-  selectedRole: Role;
-  itemCategory: ItemCategory;
-  data?: items;
+    selectedRole: Role;
+    itemCategory: ItemCategory;
+    data?: items;
 }
 
 const ItemContainer: React.FC<Props> = ({
-  selectedRole,
-  itemCategory,
-  data,
+    selectedRole,
+    itemCategory,
+    data,
 }) => {
-  if (!selectedRole || (data && data.length < 1)) {
-    return null;
-  }
+    if (!selectedRole || (data && data.length < 1)) {
+        return null;
+    }
 
-  return (
-    <div className="relative w-3/4 mx-auto mb-auto lg:w-auto group md:w-2/3">
-      <div
-        className={classNames(
-          "absolute -inset-0 blur-sm transition-all ease-in-out delay-150 duration-300 group-hover:blur-md group-hover:-inset-1 group-active:blur-md group-active:-inset-1",
-          selectedRole ? roleClasses[selectedRole].bg : undefined
-        )}
-      ></div>
-      <div
-        className={classNames(
-          "relative rounded-xl border-4 bg-white text-gray-600 py-2 drop-shadow-md",
-          selectedRole ? roleClasses[selectedRole].border : undefined
-        )}
-      >
-        <h1
-          className={classNames(
-            "mb-4 text-2xl capitalize p-2 text-white group-hover:drop-shadow-xl",
-            selectedRole ? roleClasses[selectedRole].bg : undefined
-          )}
-        >
-          {itemCategory}
-        </h1>
-        <div className="flex flex-col px-2 space-y-2">
-          {data?.map((item) => (
-            <Item item={item} key={uuidv4()} selectedRole={selectedRole} />
-          ))}
+    return (
+        <div className="relative w-3/4 mx-auto mb-auto lg:w-auto group md:w-2/3">
+            <div
+                className={classNames(
+                    "absolute -inset-0 rounded-xl blur-none transition-all ease-in-out delay-150 duration-300 group-hover:blur-sm group-hover:-inset-1 group-active:blur-sm group-active:-inset-1",
+                    selectedRole ? roleClasses[selectedRole].bg : undefined
+                )}
+            ></div>
+            <div
+                className={classNames(
+                    "relative rounded-xl border-4 bg-white text-gray-600 py-2 drop-shadow-md",
+                    selectedRole ? roleClasses[selectedRole].border : undefined
+                )}
+            >
+                <h1
+                    className={classNames(
+                        "mb-4 text-2xl capitalize p-2 text-white",
+                        selectedRole ? roleClasses[selectedRole].bg : undefined
+                    )}
+                >
+                    {itemCategory}
+                </h1>
+                <div className="flex flex-col px-2 space-y-2">
+                    {data?.map((item) => (
+                        <Item
+                            item={item}
+                            key={uuidv4()}
+                            selectedRole={selectedRole}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ItemContainer;
