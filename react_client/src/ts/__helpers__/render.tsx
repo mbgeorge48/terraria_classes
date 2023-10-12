eb996709105e0246dd3ae3479436575666e176bb
Matt George <mbgeorge48@gmail.com>
2023-10-12 15:52:25 +0100

    Add new context

--- react_client/src/ts/__helpers__/render.tsx
+++ react_client/src/ts/__helpers__/render.tsx
@@ -1,6 +1,7 @@
 import { render, RenderOptions } from "@testing-library/react";
 import { RoleProvider } from "../context/RoleContext";
 import React, { ReactElement } from "react";
+import { GameStageProvider } from "../context/GameStageContext";
 
 const consoleError = console.error;
 
@@ -15,7 +16,12 @@ afterAll(() => {
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
     return (
         <RoleProvider setSelectedRole={jest.fn()} selectedRole={"melee"}>
-            {children}
+            <GameStageProvider
+                setSelectedGameStage={jest.fn()}
+                selectedGameStage={0}
+            >
+                {children}
+            </GameStageProvider>
         </RoleProvider>
     );
 };
