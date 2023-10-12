385dffaa51514e8f42e194182fa99d040ac69c1e
Matt George <mbgeorge48@gmail.com>
2023-10-12 14:47:54 +0100

    Remove unused react import

--- react_client/src/ts/tests/credit-popover.test.tsx
+++ react_client/src/ts/tests/credit-popover.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
 import { CreditPopover } from "../credit-popover";
 
 it("should render the credits component", () => {
