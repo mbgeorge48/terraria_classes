58892a99eb3ac42ee4867327cb284d8dd6b41704
Matt George <mbgeorge48@gmail.com>
2023-12-27 14:11:50 +0000

    Move the scripts back into a subfolder

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -87,7 +87,3 @@ def deduplicate_response(response):
         item_names.append(data["name"])
         items.append(data)
     return items
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
