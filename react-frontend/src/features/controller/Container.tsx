bb94413ddf333682a3c0f4f077c836f3d45e92d5
Matt George <mbgeorge48@gmail.com>
2026-05-11 09:08:04 +0100

    Start putting together the SPA

---
+++ react-frontend/src/features/controller/Container.tsx
@@ -0,0 +1,59 @@
+import { useCallback } from "react";
+import { Slider } from "../../components/Silder/Slider";
+import { Tag, TagGroup } from "../../components/TagGroup/TagGroup";
+import { useGameStage } from "../../context/GameStageContext";
+import { useRole } from "../../context/RoleContext";
+import type { Role } from "../../types/roles";
+import { Header } from "./Heading";
+import { containerStyles } from "./styles";
+import type { Selection } from "react-aria-components";
+
+export function Container() {
+    const { selectedRole, setSelectedRole } = useRole();
+    const { selectedGameStage, setSelectedGameStage } = useGameStage();
+    console.log(selectedGameStage);
+
+    const handleRoleChange = useCallback(
+        (keys: Selection) => {
+            const newRole = [...keys][0] as Role;
+            setSelectedRole(newRole);
+        },
+        [setSelectedRole],
+    );
+
+    return (
+        <div
+            className={containerStyles({
+                selectedRole: selectedRole,
+            })}
+        >
+            <Header />
+
+            <TagGroup
+                aria-label="Role Selection"
+                onSelectionChange={handleRoleChange}
+                selectionBehavior="toggle"
+            >
+                <Tag id="melee">Melee</Tag>
+                <Tag id="ranged">Ranged</Tag>
+                <Tag id="magic">Magic</Tag>
+                <Tag id="summoner">Summoner</Tag>
+            </TagGroup>
+
+            <Slider
+                aria-label="Game Stage Selection"
+                thumbLabels={[
+                    "Pre-bosses",
+                    "Pre-Hardmode",
+                    "Pre-mech bosses",
+                    "Pre-Plantera",
+                    "Pre-Golem",
+                    "Pre-Lunar Events",
+                    "Endgame",
+                ]}
+                selectedRole={selectedRole}
+                onChange={setSelectedGameStage}
+            />
+        </div>
+    );
+}
---
+++ react-frontend/src/features/controller/styles.ts
@@ -0,0 +1,21 @@
+import { tv } from "tailwind-variants";
+
+export const containerStyles = tv({
+    base: "rounded-lg m-4 p-4 border-2 border-black space-y-4",
+    variants: {
+        selectedRole: {
+            melee: "bg-melee/20",
+            ranged: "bg-ranged/20",
+            magic: "bg-magic/20",
+            summoner: "bg-summoner/20",
+            mixed: "bg-mixed/20",
+            healer: "bg-healer/20",
+            tank: "bg-tank/20",
+            default: "bg-neutral-300/20",
+        },
+    },
+
+    defaultVariants: {
+        selectedRole: "default",
+    },
+});
