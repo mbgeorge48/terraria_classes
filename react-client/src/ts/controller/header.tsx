33912123bdec37a4816edb83ec2dec4113704e40
Matt George <mbgeorge48@gmail.com>
2026-05-12 11:41:06 +0100

    Remove the old deprecated code

--- react-client/src/ts/controller/header.tsx
+++
@@ -1,9 +0,0 @@
-export function Header() {
-    return (
-        <header className="flex flex-row justify-center p-4 m-2 space-x-2 text-3xl font-bold capitalize bg-white lg:justify-start lg:space-x-0 lg:flex-col lg:max-w-min rounded-xl">
-            <h1>terraria</h1>
-            <h1>classes</h1>
-            <h1>guide</h1>
-        </header>
-    );
-}
