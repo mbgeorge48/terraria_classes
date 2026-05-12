33890dc1fa2101f1d6d55a9a6cf565d7320ce25a
Matt George <mbgeorge48@gmail.com>
2026-05-12 17:13:32 +0100

    Add support for an env file

--- react-frontend/src/api/useApi.ts
+++ react-frontend/src/api/useApi.ts
@@ -2,9 +2,7 @@ import useSWR, { type SWRResponse } from "swr";
 
 import { fetcher } from "./fetcher";
 
+const apiUrl = import.meta.env.VITE_API_URL;
 export function useApi<T>(params: string | null): SWRResponse<T, Error> {
-    return useSWR<T>(
-        params ? `http://localhost:8000/${params}` : null,
-        fetcher,
-    );
+    return useSWR<T>(params ? `${apiUrl}/${params}` : null, fetcher);
 }
